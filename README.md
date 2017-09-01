7a1028c904a865b345ea52822a835fd6dbc256c0
John Dickinson <john.dickinson@enta.net>
2017-09-01 11:31:19 +0100

    updating readme with useful info

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # lorum
 PHP Package - Random Text Generator
+
+Use as filler text or just for fun. Works as a Composer Package or standalone.
+
+For Composer usage:
+
+```php
+<?php
+require "vendor/autoload.php";
+
+use Dickinsonjl\Lorum\Lorum;
+
+
+$lorum = new Lorum();
+$lorum->buildCache(); // build text Catalogue from LorumSeed file
+
+
+echo $lorum->giveMeWord(1) . PHP_EOL; // specify word length as argument
+echo $lorum->giveMeWord(2) . PHP_EOL;
+echo $lorum->giveMeWord(3) . PHP_EOL;
+echo $lorum->giveMeWord(4) . PHP_EOL;
+echo $lorum->giveMeWord(5) . PHP_EOL;
+echo $lorum->giveMePhrase() . PHP_EOL;
+echo $lorum->giveMeSentence() . PHP_EOL;
+echo $lorum->giveMeSentence(1) . PHP_EOL; // specify number of phrases for the sentence
+echo $lorum->giveMeParagraph() . PHP_EOL;
+echo $lorum->giveMeMultiParagraph(5) . PHP_EOL; // specify number of paragraphs
+```
+
+Project [on GitHub](https://github.com/dickinsonjl/lorum).
+
+Composer [Package](https://packagist.org/packages/dickinsonjl/lorum)
+
--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <?php
-// require "src/Lorum.php";
 require "vendor/autoload.php";
 
 use Dickinsonjl\Lorum\Lorum;
 
 
 $lorum = new Lorum();
-$lorum->buildCache();
+$lorum->buildCache(); // build text Catalogue from LorumSeed file
 
-// var_dump($lorum->wordPool);
-// var_dump($lorum->wordsPerPhraseFrequency);
-// var_dump($lorum->phrasesPerSentenceFrequency);
-// var_dump($lorum->wordLengthFrequency);
-// echo $lorum->giveMeWord(4) . PHP_EOL;
+
+echo $lorum->giveMeWord(1) . PHP_EOL; // specify word length as argument
+echo $lorum->giveMeWord(2) . PHP_EOL;
+echo $lorum->giveMeWord(3) . PHP_EOL;
+echo $lorum->giveMeWord(4) . PHP_EOL;
+echo $lorum->giveMeWord(5) . PHP_EOL;
 echo $lorum->giveMePhrase() . PHP_EOL;
-// echo $lorum->giveMeSentence() . PHP_EOL;
-// echo $lorum->giveMeParagraph() . PHP_EOL;
-// echo $lorum->giveMeMultiParagraph(5) . PHP_EOL;
+echo $lorum->giveMeSentence() . PHP_EOL;
+echo $lorum->giveMeSentence(1) . PHP_EOL; // specify number of phrases for the sentence
+echo $lorum->giveMeParagraph() . PHP_EOL;
+echo $lorum->giveMeMultiParagraph(5) . PHP_EOL; // specify number of paragraphs
